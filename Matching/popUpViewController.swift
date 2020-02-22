8a951ecb5105f7d800bcec8a9cb1e49b4e132001
rhkwj <rhkwj@naver.com>
2020-02-22 15:20:31 +0800

    make matching score

--- Matching/popUpViewController.swift
+++ Matching/popUpViewController.swift
@@ -1,30 +1,21 @@
 //
-//  Person2ViewController.swift
-//  AllenShit
+//  popUpViewController.swift
+//  Matching
 //
-//  Created by Kim Yeon Jeong on 2020/2/20.
+//  Created by Kim Yeon Jeong on 2020/2/22.
 //  Copyright © 2020 Kim Yeon Jeong. All rights reserved.
 //
 
 import UIKit
 
-class Person2ViewController: UIViewController {
+class popUpViewController: UIViewController {
 
-
-    @IBAction func popUpPressed(_ sender: Any) {
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // Do any additional setup after loading the view.
-        
 
-    
+        // Do any additional setup after loading the view.
     }
     
-
 
     /*
     // MARK: - Navigation
@@ -35,8 +26,5 @@ class Person2ViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    @IBAction func goBackToOneButtonTapped(_ sender: Any) {     performSegue(withIdentifier: "unwindSegueToResult", sender: self)}
 
-    
-    
 }
